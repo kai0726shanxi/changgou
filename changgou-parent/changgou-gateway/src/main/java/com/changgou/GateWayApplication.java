412f1b38711936a4710633456055b525953abe40
gaokai <569968571@qq.com>
2020-06-01 22:57:19 +0800

    项目构建

---
+++ changgou-parent/changgou-gateway/src/main/java/com/changgou/GateWayApplication.java
@@ -0,0 +1,19 @@
+package com.changgou;
+
+/*
+ *功能描述
+ * @author gao
+ * @date 2020-06-01
+ * @description 网关
+ */
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+    }
+}
---
+++ changgou-parent/changgou-service/changgou-product-service/src/main/java/com/changgou/ProductApplication.java
@@ -0,0 +1,20 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/*
+ *功能描述
+ * @author gao
+ * @date 2020-06-01
+ * @description 项目入口$
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
